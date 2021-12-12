76d179516dfd0ea53e82c699cb4d3da0a8df1b06
Danilo Casimiro <dan_casimiro@hotmail.com>
2021-12-11 22:56:02 -0300

    Adicionado na busca por todos a possibilidade de enviar parametros para refinar a busca

--- services/systemServices/GetObjectsService.php
+++ services/systemServices/GetObjectsService.php
@@ -2,10 +2,38 @@
 
 namespace app\services\systemServices;
 
+use Yii;
+
 class GetObjectsService {
 
   public static function getObjects(string $class): object
   {
-    return $class::find()->where(['deleted_at' => null]);
+    
+    $query =  $class::find()->where(['deleted_at' => null]);
+    static::getExtraQueries($class, $query);
+    
+    return $query;
+  }
+
+  private static function getExtraQueries($class, &$query)
+  {
+    $queryParams = Yii::$app->request->get();
+
+    $model = new $class;
+
+    $attributes = $model->getAttributes();
+
+    if(!empty($queryParams)) {
+      
+      foreach($queryParams as $key => $value) {
+        
+        if(array_key_exists($key, $attributes)) {
+          
+          $query->where([
+            'like', $key,  "%".$value."%", false
+          ]);
+        }
+      }
+    } 
   }
 }
