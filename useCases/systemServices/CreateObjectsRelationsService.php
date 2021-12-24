72821b19dcc3482543d32672d929e8e71ef05d00
Danilo Casimiro <dan_casimiro@hotmail.com>
2021-12-24 12:01:06 -0300

    Ajsutado o arquivo CreateObjectsRelationsService para aceitar salvamento com array de relations

--- useCases/systemServices/CreateObjectsRelationsService.php
+++ useCases/systemServices/CreateObjectsRelationsService.php
@@ -15,28 +15,48 @@ class CreateObjectsRelationsService {
         if(!empty($relations) && !empty($model->fkAttribute())) {
             $transaction = Yii::$app->db->beginTransaction();
 
-            $fkAttribute = $model->fkAttribute(); 
-
             foreach($relations as $relation => $class) {
-                
+
                 if(!empty($params[$relation])) {
-
-                    $modelRelation = new $class;
-
-                    $modelRelation->load($params[$relation], '');
-                   
-                    $modelRelation->$fkAttribute = $model->id;
-
-                    if(!$modelRelation->save()) {
-
-                        $transaction->rollBack();
-
-                        throw new BadRequestHttpException(json_encode($model->getErrors()));
-                    }
+                    static::prepareObjectRelation($params[$relation], $class, $model); 
                 }
             }
 
             $transaction->commit();
         }
+    }
+
+    private static function prepareObjectRelation(array $relationParams, string $class, ModelInterface $model): void
+    {
+        $arrayRelations = array_key_exists(0, $relationParams);
+
+        if($arrayRelations) {
+            foreach($relationParams as $relationParam) {
+                static::saveObjectRelation($relationParam, $model, $class);
+            }
+
+            return;
+        }
+
+        static::saveObjectRelation($relationParams, $model, $class);
+        
+    }
+
+    private static function saveObjectRelation(array $relationParams, ModelInterface $model, $class): void
+    {
+        $modelRelation = new $class;
+
+        $fkAttribute = $model->fkAttribute(); 
+
+        $modelRelation->load($relationParams, '');
+        
+        $modelRelation->$fkAttribute = $model->id;
+
+        if(!$modelRelation->save()) {
+
+            throw new BadRequestHttpException(json_encode($model->getErrors()));
+        }
+
+        static::createObjectsRelations($modelRelation, $relationParams);
     }
 }
